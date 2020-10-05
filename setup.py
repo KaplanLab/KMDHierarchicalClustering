94aa22d7f9937ec149222e037981d70e6904b3af
Maarten-vd-Sande <maartenvandersande@hotmail.com>
2020-10-05 14:53:40 +0200

    does this work?

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="KMDHierarchicalClustering", # Replace with your own username
+    name="KMDHierarchicalClustering",  # Replace with your own username
     version="0.0.0",
     author="avivzelig",
     author_email="author@example.com",
